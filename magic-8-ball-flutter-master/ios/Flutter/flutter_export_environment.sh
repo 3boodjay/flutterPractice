33ba53e1456360e928ff822726c0dea288385606
3boodjay <60220670+3boodjay@users.noreply.github.com>
2020-03-03 09:05:33 +0300

    words instead of image

--- magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
+++ magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/abdullaaljalahma/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/abdullaaljalahma/Udemy/flutter/magic-8-ball-flutter-master"
-export "FLUTTER_TARGET=/Users/abdullaaljalahma/Udemy/flutter/magic-8-ball-flutter-master/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/abdullaaljalahma/Documents/GitHub/flutterPractice/magic-8-ball-flutter-master"
+export "FLUTTER_TARGET=/Users/abdullaaljalahma/Documents/GitHub/flutterPractice/magic-8-ball-flutter-master/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/abdullaaljalahma/Developer/flutter/bin/cache/artifacts/engine/ios"
--- magic-8-ball-flutter-master/lib/main.dart
+++ magic-8-ball-flutter-master/lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:shake/shake.dart';
 
+const kBurgany = Color(0xFF4D0403);
+const kNavyBlue = Color(0xFF182855);
+
+int ballindex = 1;
+
+List<String> choices = [
+  "Sit on face",
+  "Eat pussy",
+  "Suck dick",
+  "Finger",
+  "Make out",
+  "Eat ass",
+  "Kiss neck",
+  "Get it on",
+];
+
 void main() => runApp(
       MaterialApp(home: Scaffoldcode()),
     );
@@ -10,17 +26,20 @@ class Scaffoldcode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: kNavyBlue,
       appBar: AppBar(
+        backgroundColor: kBurgany,
         title: Text(
-          'shake me',
+          'shake me down :)',
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
       body: Eightball(),
-      backgroundColor: Colors.black,
     );
   }
 }
+
+//shake me ,':)
 
 class Eightball extends StatefulWidget {
   @override
@@ -28,22 +47,14 @@ class Eightball extends StatefulWidget {
 }
 
 class _EightballState extends State<Eightball> {
-  int ballindex = 1;
+  void initState() {
+    super.initState();
+  }
 
   void shakeball() {
     setState(() {
-      ballindex = Random().nextInt(5) + 1;
+      ballindex = Random().nextInt(8);
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
-      shakeball();
-    });
-     detector.startListening();
-    // TODO: fix the shaking BOY!!!
   }
 
   @override
@@ -51,25 +62,52 @@ class _EightballState extends State<Eightball> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        FlatButton(
-            onPressed: () {
-              shakeball();
-            },
-            child: Image.asset("images/ball$ballindex.png")),
+        Stack(children: <Widget>[
+          Image.asset("images/ball1.png"),
+          Positioned(
+            bottom: 130,
+            right: 160,
+            child: Container(
+              width: 80.0,
+              height: 100.0,
+              child: Text(
+                choices[ballindex],
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ]),
         SizedBox(
           height: 20,
         ),
         RaisedButton(
           onPressed: () {
             shakeball();
+            ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+              shakeball();
+            });
           },
           child: Text(
             "shake me",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          color: Colors.blue,
+          color: kBurgany,
         )
       ],
     );
   }
 }
+
+//@override
+//void initState() {
+//super.initState();
+//ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+//shakeball();
+//});
+//detector.startListening();
+// TODO: fix the shaking BOY!!!
+//}
